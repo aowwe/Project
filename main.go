d7f70dfb103485aa47a6737350e946cbbf0b0211
Apple <apple@MacBook-Pro-oa.local>
2023-05-17 19:20:34 +0100

    i finishgit add .

--- main.go
+++ main.go
@@ -10,10 +10,17 @@ import (
 
 func main() {
 	data.FetchAllRecipes()
+
 	server := http.NewServeMux()
+
 	style := http.FileServer(http.Dir("frontend/css"))
+
 	server.Handle("/frontend/css/", http.StripPrefix("/frontend/css/", style))
+
 	server.HandleFunc("/", handlers.HomePage)
+	server.HandleFunc("/recipe/", handlers.RecipePage)
+
 	fmt.Println(data.AllRecipes)
+
 	log.Fatal(http.ListenAndServe(":8000", server))
 }
